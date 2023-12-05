0172cec434f41210378aa634307d2b86f782b503
Arghya Pal <89515522+arghyapal15@users.noreply.github.com>
2023-12-05 23:53:58 +0530

    Create 13. Maximum Subarray Sum.cpp

---
+++ 13. Maximum Subarray Sum.cpp
@@ -0,0 +1,30 @@
+long long maxSubarraySum(vector<int> arr, int n)
+
+{
+
+    long long maxsum = 0;
+
+    long long currsum = 0;
+
+    for(int i=0; i<n; i++){
+
+        currsum +=arr[i];
+
+        if(currsum > maxsum){
+
+            maxsum = currsum;
+
+        }
+
+        if(currsum<0){
+
+            currsum = 0;
+
+        }
+
+    }
+
+    return maxsum;
+
+}
+
