d4377310f0886de531ec777e4a9187fcfcb7a42d
Arghya Pal <89515522+arghyapal15@users.noreply.github.com>
2023-12-05 23:55:42 +0530

    Create 14. Best Time to Buy and Sell Stock.cpp

---
+++ 14. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+
+int maximumProfit(vector<int> &prices){
+
+    int buy=INT_MAX;
+
+    int profit=0;
+
+    for(int i=0;i<prices.size();i++)
+
+    {
+
+        if(buy > prices[i]) buy = prices[i];
+
+        else if (prices[i]-buy>profit) profit= prices[i]-buy;
+
+    }
+
+    return profit;
+
+}
